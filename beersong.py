e6cac719a4806455b4f27bc154501455c909d1cc
mblaul <mblaul@oakland.edu>
2018-01-27 15:10:13 -0500

    Added logic for 99 beer bottles function. Follows grammatical rules.

--- beersong.py
+++ beersong.py
@@ -1,5 +1,25 @@
+## author Matt Blaul
+## 1/26/2018
+## CSI 3370
 def main():
 
+
 def ninety_nine_bottles_of_beer():
+    numbottles = 99
+
+    for x in range(numbottles, 0, -1):
+        if (x > 1):
+            print(str(x) + " bottles of beer on the wall, " + str(x) + " bottles of beer")
+            print("take	one	down, pass it around, " + str(x-1) + " bottles of beer on the wall.")
+        else:
+            print("1 bottle of beer on the wall, 1 bottle of beer")
+            print("take	one	down, pass it around, no more bottles of beer on the wall.")
+
 
 main()
+
+##DESIRED OUTPUT
+# 2	bottles	of	beer	on	the	wall,	2	bottles	of	beer
+# take	one	down,	pass	it	around,	1	bottle	of	beer	on	the	wall.
+# 1	bottle	of	beer	on	the	wall,	1	bottle	of	beer
+# take	one	down,	pass	it	around,	no	more	bottles	of	beer	on	the	wall.
